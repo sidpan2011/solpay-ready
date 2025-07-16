81a042888ad76c194a927fff9349b38901779f22
Sidhanth Pandey <sidhanthpandey@192.168.1.6>
2025-07-17 01:37:46 +0530

    feat: add ui components

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+
+export default function Footer() {
+    return (
+        <footer className="flex justify-between items-center p-4 bg-">
+            <div className="max-w-6xl mx-auto">
+                <div className="">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo voluptatum accusantium debitis, dolor vitae officiis unde nobis. Vitae nulla eveniet fuga! Laborum, rem atque quas quaerat reprehenderit magnam libero perspiciatis?</p>
+                </div>
+            </div>
+        </footer>
+    );
+}
